815c9468530f6e4d327ace4c600d288eab8ea68e
Vitor Rodrigues Ferreira <vitor.ferreira@rocketwave.tech>
2021-10-24 20:17:17 -0300

    Habilita configuração do swagger no projeto

---
+++ src/main/java/br/com/sigabem/configuration/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package br.com.sigabem.configuration;
+
+import org.springframework.context.annotation.*;
+import springfox.documentation.builders.*;
+import springfox.documentation.spi.*;
+import springfox.documentation.spring.web.plugins.*;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
